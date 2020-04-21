1191a5a7c88476ee42017a4f19ece985e9690c01
Ana Olivares <anaolivares2712@gmail.com>
2020-04-21 03:07:58 -0300

    Test no menos de 15

--- tests/testceldas.py
+++ tests/testceldas.py
@@ -1,1 +1,9 @@
+from src.bingo import carton
 
+def test_no_menos_15():
+    mi_carton = carton()
+    cont = 0
+    for fila in mi_carton:
+        for celda in fila:
+            cont += celda
+    assert cont >= 15
