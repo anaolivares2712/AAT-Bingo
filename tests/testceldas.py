b9d6742f7618caf8dd1c79148ed64c553fe15f52
Ana Olivares <anaolivares2712@gmail.com>
2020-04-21 03:43:43 -0300

    Test al menos una celda ocupada por columna

--- tests/testceldas.py
+++ tests/testceldas.py
@@ -15,3 +15,11 @@ def test_no_mas_15():
         for celda in fila:
             cont += celda
     assert cont <= 15
+
+def test_columnas():
+    mi_carton = carton()
+    cont = 0
+    for i in range(9):
+        if(mi_carton[0][i] == 1 or mi_carton[1][i] == 1 or mi_carton[2][i] == 1):
+            cont += 1
+    assert cont == 9
