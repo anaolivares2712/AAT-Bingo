b5c195bb6846c0cfe27c4c000e0216afc3a50664
anaolivares2712 <anaolivares2712@gmail.com>
2020-06-08 22:29:17 -0300

    Ingreso solo una vez mi_carton

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -1,7 +1,8 @@
 from src.bingo import carton
 
+mi_carton = carton()
+
 def test_no_menos_15():
-    mi_carton = carton()
     cont = 0
     for fila in mi_carton:
         for celda in fila:
@@ -9,7 +10,6 @@ def test_no_menos_15():
     assert cont >= 15
 
 def test_no_mas_15():
-    mi_carton = carton()
     cont = 0
     for fila in mi_carton:
         for celda in fila:
@@ -17,7 +17,6 @@ def test_no_mas_15():
     assert cont <= 15
 
 def test_columnas():
-    mi_carton = carton()
     cont = 0
     for i in range(9):
         if(mi_carton[0][i] == 1 or mi_carton[1][i] == 1 or mi_carton[2][i] == 1):
@@ -25,7 +24,6 @@ def test_columnas():
     assert cont == 9
 
 def test_filas():
-    mi_carton = carton()
     cont = 0
     for i in range(3):
         for j in range(9):
