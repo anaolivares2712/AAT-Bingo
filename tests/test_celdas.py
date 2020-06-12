187541cf960e86f13aca53474c6f66b14c95ee71
anaolivares2712 <anaolivares2712@gmail.com>
2020-06-12 04:52:06 -0300

    Arreglo mi_carton

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -1,10 +1,10 @@
-from src.bingo import nuevo_carton
+from src.bingo import carton
 from src.bingo import no_mas_15
 from src.bingo import no_menos_15
 from src.bingo import elementos_columnas
 from src.bingo import elementos_filas
 
-mi_carton = nuevo_carton()
+mi_carton = carton
 
 def test_no_menos_15():
     assert no_menos_15(mi_carton) >= 15
