e84d0b9b8a678e6c430932687332ce537188fb58
anaolivares2712 <anaolivares2712@gmail.com>
2020-07-01 02:45:40 -0300

    Elimino mi_carton2

--- tests/test_numeros.py
+++ tests/test_numeros.py
@@ -15,28 +15,13 @@ def test_entre_1_y_90():
     assert entre_1_y_90(mi_carton) == 0
 
 def test_aumenta_hacia_la_derecha():
-    mi_carton2 = (
-        (0,10,0,33,43,0,68,0,81),
-        (0,18,22,0,49,52,0,70,0),
-        (7,0,26,0,0,57,0,78,84)
-    )
-    assert aumentan_hacia_la_derecha(mi_carton2) == 0
+    assert aumentan_hacia_la_derecha(mi_carton) == 0
 
 def test_aumenta_hacia_abajo():
-    mi_carton2 = (
-        (0,10,0,33,43,0,68,0,81),
-        (0,18,22,0,49,52,0,70,0),
-        (7,0,26,0,0,57,0,78,84)
-    )
-    assert aumentan_hacia_abajo(mi_carton2) == 0
+    assert aumentan_hacia_abajo(mi_carton) == 0
 
 def test_numeros_unicos():
-    mi_carton2 = (
-        (3,0,22,0,41,52,0,0,89),
-        (7,12,0,38,0,0,0,73,0),
-        (0,18,25,0,44,57,64,76,0)
-    )
-    assert numeros_unicos(mi_carton2) == 0
+    assert numeros_unicos(mi_carton) == 0
 
 def test_cant_celdas_ocupadas():
     assert cant_celdas_ocupadas(mi_carton) == 0
