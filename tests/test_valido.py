be537e7d977e6632027d7f43f65ae72e93c4c0e2
anaolivares2712 <anaolivares2712@gmail.com>
2020-07-05 16:33:10 -0300

    Creo archivo con todas las funciones que validan el carton

---
+++ tests/test_valido.py
@@ -0,0 +1,42 @@
+from src.bingo import *
+
+mi_carton = carton
+
+def test_no_menos_15():
+    assert no_menos_15(mi_carton) >= 15
+
+def test_no_mas_15():
+    assert no_mas_15(mi_carton) <=15
+
+def test_elementos_columnas():
+    assert elementos_columnas(mi_carton) == 9
+
+def test_elementos_filas():
+    assert elementos_filas(mi_carton) == 3
+
+def test_entre_1_y_90():
+    assert entre_1_y_90(mi_carton) == 0
+
+def test_aumenta_hacia_la_derecha():
+    assert aumentan_hacia_la_derecha(mi_carton) == 0
+
+def test_aumenta_hacia_abajo():
+    assert aumentan_hacia_abajo(mi_carton) == 0
+
+def test_numeros_unicos():
+    assert numeros_unicos(mi_carton) == 0
+
+def test_cant_celdas_ocupadas():
+    assert cant_celdas_ocupadas(mi_carton) == 0
+
+def test_matriz():
+    assert matriz(mi_carton) == 0
+
+def test_columnas_tres_celdas_ocupadas():
+    assert columnas_tres_celdas_ocupadas(mi_carton) == 0
+
+def test_celdas_vacias_consecutivas():
+    assert celdas_vacias_consecutivas(mi_carton) == 0
+
+def test_celdas_ocupadas_consecutivas():
+    assert celdas_ocupadas_consecutivas(mi_carton) == 0
