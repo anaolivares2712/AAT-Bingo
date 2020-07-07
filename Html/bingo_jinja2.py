0106dca7f8abf73cbcfd623b441b582b5ec797f0
anaolivares2712 <anaolivares2712@gmail.com>
2020-07-07 20:09:40 -0300

    Completo código

--- Html/bingo_jinja2.py
+++ Html/bingo_jinja2.py
@@ -2,3 +2,7 @@ from jinja2 import Template
 from src.bingo import tests
 
 template = Template(open("Html/plantilla.j2").read())
+file = open("bingo.html", "w")
+file.write(template.render(carton = tests()))
+file.close()
+print("Creando \"bingo.html\".")
